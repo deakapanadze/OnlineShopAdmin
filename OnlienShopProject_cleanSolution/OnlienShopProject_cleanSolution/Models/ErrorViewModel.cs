52128f0d2dea24fec508a6d574342c5d61fdbadc
dea <60604720+deakapanadze@users.noreply.github.com>
2022-12-17 23:52:46 +0400

    Delete OnlienShopProject_cleanSolution directory

--- OnlienShopProject_cleanSolution/OnlienShopProject_cleanSolution/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlienShopProject_cleanSolution.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
