7bcd78642bda883af06794bf4fe4e925d84707bd
dea <60604720+deakapanadze@users.noreply.github.com>
2022-12-17 23:53:42 +0400

    Delete DemoMvcArcitecture directory

--- DemoMvcArcitecture/DemoMvcArcitecture/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoMvcArcitecture.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
